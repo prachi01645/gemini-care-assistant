bc2f566049dea7b4d513146c24da0498560dd084
prachi01645 <prachipatel0848@gmail.com>
2026-03-22 12:11:45 +0530

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,9 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBfyZbzOj5sRHW1vevYSR_aTRGWDp3o6C0")
+
+model = genai.GenerativeModel("gemini-pro")
+
+response = model.generate_content("Say hello")
+
+print(response.text)

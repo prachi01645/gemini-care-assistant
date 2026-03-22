892ca9db54bacb5583fc811f5962d4d109d784b1
prachi01645 <prachipatel0848@gmail.com>
2026-03-22 12:24:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyBfyZbzOj5sRHW1vevYSR_aTRGWDp3o6C0")
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 response = model.generate_content("Say hello")
 
